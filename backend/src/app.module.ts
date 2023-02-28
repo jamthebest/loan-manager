2765fc02f7f5fa3cd3ab9eedb57377e94327df98
Javier Midence <javier.midence22@gmail.com>
2023-02-28 15:13:14 -0600

    Generating resources

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { LoansModule } from './loans/loans.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ContactsModule, LoansModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
